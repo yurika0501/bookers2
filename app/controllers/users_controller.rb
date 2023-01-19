57fb8386f39e415732179b6908bfd65409ddb814
EC2 Default User <ec2-user@ip-172-31-47-250.ap-northeast-1.compute.internal>
2023-01-19 11:34:49 +0000

    [Add]ActiveStorageの導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:image, :name,)
+    params.require(:user).permit(:title, :body, :image)  
+    
   end
 end
