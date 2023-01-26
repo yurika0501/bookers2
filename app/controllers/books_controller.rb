3282219910a43d447cbe9f1605df0bfca8d05f01
EC2 Default User <ec2-user@ip-172-31-47-250.ap-northeast-1.compute.internal>
2023-01-26 06:29:20 +0000

    [Add]RSpecテストエラー修正中3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,12 +23,13 @@ class BooksController < ApplicationController
   # end
   
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
+    @newbook = Book.new(book_params)
+    @newbook.user_id = current_user.id
+    if @newbook.save
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@newbook.id)
     else
+      @user = User.find(current_user.id)
       @books = Book.all
       render :index
     end
